b5637806c564891436731dc61733046dd2df35ae
core.editor <soumen@example.com>
2025-06-12 14:06:58 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'export',
+  basePath: '/sayan-portfolio',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
